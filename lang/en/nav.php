491ab64f55af6e98eb0953aad320b2afae444866
tqt97 <kutuanonline199@gmail.com>
2025-05-24 15:41:50 +0700

    add lang

---
+++ lang/en/nav.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'logout' => 'Logout',
+    'login' => 'Login',
+    'register' => 'Register',
+    'home' => 'Home',
+    'about' => 'About',
+    'services' => 'Services',
+    'contact' => 'Contact',
+    'categories' => 'Categories',
+    'posts' => 'Posts',
+    'tags' => 'Tags',
+];
