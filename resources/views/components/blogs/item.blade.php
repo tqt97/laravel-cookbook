d6eb9cbcc2f0e1aa91caecdf81472099d9592ba5
tqt97 <kutuanonline199@gmail.com>
2025-05-27 20:46:18 +0700

    Add code

--- resources/views/components/blogs/item.blade.php
+++ resources/views/components/blogs/item.blade.php
@@ -3,26 +3,33 @@
 <div
     class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 overflow-hidden border border-gray-100">
     <div class="p-6">
-        <h2 class="text-xl font-semibold mb-3 line-clamp-2 min-h-[3.5rem]">
-            <a href="{{ route('posts.show', $post) }}">
+        <h2 class="text-xl font-semibold mb-2 line-clamp-2 min-h-[3.5rem]">
+            <a href="{{ route('posts.show', $post) }}" title="{{ $post->title}}">
                 {{ $post->title}}
             </a>
         </h2>
 
-        <div class="flex items-center gap-4 text-sm text-gray-500 mb-4">
-            <div class="flex items-center gap-1">
-                <x-icons.tag />
-                <span>{{ $post->category->name }}</span>
+        <div class="flex items-center justify-between gap-4 text-sm text-gray-5001 mb-2">
+            <div class="flex gap-1">
+                <x-icons.user-circle />
+                <span>{{ $post->user->name }}</span>
             </div>
-            <div class="flex items-center gap-1">
-                <x-icons.message />
-                <span>1</span>
-            </div>
-            <div class="flex items-center gap-1">
+            <div class="flex gap-1">
                 <x-icons.calendar />
-                <span>{{ $post->created_at->diffForHumans() }}</span>
+                <span>{{ $post->created_at->format('Y-m-d') }}</span>
             </div>
         </div>
+        <div class="flex  justify-between text-sm my-2">
+            <div class="flex gap-1">
+                <x-icons.time />
+                {{ Str::readingTime($post->content) }}
+            </div>
+            <div class="flex gap-1">
+                <x-icons.book-open />
+                <span>{{ $post->category->name }}</span>
+            </div>
+        </div>
+
 
         <div class="text-gray-600 mb-4 line-clamp-2">
             {!! $post->content !!}
